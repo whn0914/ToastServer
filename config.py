0e247f9c3d465573ecc4ff55f36160ceed02671f
whn0914 <whn0914@gmail.com>
2016-01-19 14:34:26 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-  
-
-dbhost = '218.244.132.65:3306' 
-dbuser = 'root'
-dbpassword = '920704'  
-dbname = 'toast_db'  
-  
-SQLALCHEMY_DATABASE_URI = 'mysql://' + dbuser + ':' + dbpassword + '@' + dbhost + '/' +dbname
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
